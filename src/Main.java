5abb589dd450f0f0eec1ba70014f1d851e68e5bf
YIDANA VICTOR <vyidana@st.ug.edu.gh>
2026-06-16 17:45:55 +0000

    Implement linear and binary search in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target value: ");
+        int target = sc.nextInt();
+
+        // Linear Search
+        int linearResult = Algorithm.linearSearch(array, target);
+
+        System.out.println("\nLinear Search Result:");
+        if (linearResult != -1) {
+            System.out.println("Target found at index " + linearResult);
+        } else {
+            System.out.println("Target not found");
+        }
+
+        // Binary Search (must sort first)
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+        Arrays.sort(sortedArray);
+
+        int binaryResult = Algorithm.binarySearch(sortedArray, target);
+
+        System.out.println("\nBinary Search Result:");
+        if (binaryResult != -1) {
+            System.out.println("Target found at index " + binaryResult);
+        } else {
+            System.out.println("Target not found");
+        }
+
+        sc.close();
     }
 }
