d2a170f61b13c69151d3c7a4ebe9538750b4ebad
YIDANA VICTOR <vyidana@st.ug.edu.gh>
2026-06-16 17:12:45 +0000

    Add linear and binary search methods

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,2 +1,24 @@
-public class Algorithms {
+public class Algorithm {
+
+    // Linear Search method
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) return i;
+        }
+        return -1;
+    }
+
+    // Binary Search method
+    public static int binarySearch(int[] array, int target) {
+        int left = 0, right = array.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (array[mid] == target) return mid;
+            else if (array[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
 }
